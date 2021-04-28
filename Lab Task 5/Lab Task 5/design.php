105fba7a0c48c138947baac52fa301f7670f54a0
Afsanakhan122 <70193030+Afsanakhan122@users.noreply.github.com>
2021-04-28 12:00:37 +0600

    Add files via upload

---
+++ Lab Task 5/Lab Task 5/design.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	      <style>
+     .error {color: #FF0000;}
+
+  header {
+  background-color: #DB7093;
+  padding: 40px;
+  text-align: center;
+  font-size: 20px;
+  color: white;
+
+} 
+  footer {
+  background-color: #777;
+  padding: 10px;
+  text-align: center;
+  color: white;
+}
+.main {
+  margin-left: 600px; 
+  font-size: 20px; 
+  padding: 0px 20px;
+}
+     </style>
+</head>
+<body>
+
+</body>
+</html>
